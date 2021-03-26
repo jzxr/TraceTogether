c872aeb5dc78fa3462cea7cea0bd231b1dcd3e3b
jzxr <57856990+jzxr@users.noreply.github.com>
2021-03-26 19:30:38 +0800

    Create graph2.py

---
+++ graph2.py
@@ -0,0 +1,165 @@
+def dijkstra(graph,src,dest,visited=[],distances={},predecessors={}):
+    """ calculates a shortest path tree routed in src
+    """    
+    # a few sanity checks
+    if src not in graph:
+        raise TypeError('The root of the shortest path tree cannot be found')
+    if dest not in graph:
+        raise TypeError('The target of the shortest path cannot be found')    
+    # ending condition
+    if src == dest:
+        # We build the shortest path and display it
+        path=[]
+        pred=dest
+        while pred != None:
+            path.append(pred)
+            pred=predecessors.get(pred,None)
+        # reverses the array, to display the path nicely
+        readable=path[0]
+        for index in range(1,len(path)): readable = path[index]+'--->'+readable
+        #prints it 
+        print('shortest path - array: '+str(path))
+        print("path: "+readable+",   distance="+str(distances[dest]))    
+    else :     
+        # if it is the initial  run, initializes the cost
+        if not visited: 
+            distances[src]=0
+        # visit the neighbors
+        for neighbor in graph[src] :
+            if neighbor not in visited:
+                new_distance = distances[src] + graph[src][neighbor]
+                if new_distance < distances.get(neighbor,float('inf')):
+                    distances[neighbor] = new_distance
+                    predecessors[neighbor] = src
+        # mark as visited
+        visited.append(src)
+        # now that all neighbors have been visited: recurse                         
+        # select the non visited node with lowest distance 'x'
+        # run Dijskstra with src='x'
+        unvisited={}
+        for k in graph:
+            if k not in visited:
+                unvisited[k] = distances.get(k,float('inf'))        
+        x=min(unvisited, key=unvisited.get)
+        dijkstra(graph,x,dest,visited,distances,predecessors)
+        
+
+
+if __name__ == "__main__":
+
+    graph = {'82863770': {'99053469': 2, '98741636': 1},
+            '99053469': {'82863770': 3, '98741636': 4, '99995219':8},
+            '98741636': {'82863770': 4, '99053469': 2, '98173219': 2},
+            '99995219': {'99053469': 2, '85991110': 7, '97604430': 4},
+            '85991110': {'98741636': 1, '99995219': 11, '97604430': 5},
+            '97604430': {'85991110': 3, '85991110': 5}}
+    dijkstra(graph,'82863770','97604430')
+
+# import networkx as nx
+# import matplotlib.pyplot as plt
+
+#graph will be based on 20/01/2021
+
+    # '99053469' : {'98741636' : 27},
+    # '82863770' : {},
+    # '93213933' : {'99995219': 50, '98439577': 32, '85991110': 4},
+    # '91700369' : {'99995219': 50, '98439577': 32, '85991110': 4},
+    # '93213933' : {},
+    # '98173219' : {},
+    # '91700369' : {'97604430' : 2, '99995219' : 2, '97601182' : 2, '86148198' : 2, '88877112' : 2, '91654703' : 44},
+    # # "Cathay Cineleisure Orchard" : {"91164088" : 11},
+    # "Capitol Singapore" : {"98114818" : 27, "92561060" : 21},
+    # "City Square Mall" : {"90175180" : 24, "94459175" : 4, "97604430" : 4,  "88877112" : 1, "95749866" : 1},
+    # "Duo" : {"94185021" : 3},
+    # "Far East Plaza" : {"92245161" : 9, "90782389" : 21},
+    # "Funan" : {"86806044" : 11, "96625428" : 4, "98114818" : 1, "92561060" : 1},
+    # "Gek Poh Shopping Centre" : {"90782389" : 11},
+    # "Great World City" : {"94185021" : 27},
+    # "HDB Hub" : {"98173219" : 4, "87686851" : 5},
+    # "ION Orchard" : {"83858992" : 19},
+    # "IMM" : {"97936255" : 6, "98173219" : 6, "90515712" : 6, "88539489" : 11},
+    # "Jcube" : {"99002814" : 1, "90782389" : 1},
+    # "Jem" : {"98741632" : 8, "96301245" : 3},
+    # "Jewel Changi" : {"91806790" : 27},
+    # "Junction 8" : {"87686851" : 3},
+    # "Jurong Point" : {"98741631" : 1, "98741632" : 4, "96301245" : 1, "85571447" : 1},
+    # "Liang Court" : {"82116909" : 1},
+    # "Lucky Plaza" : {"97936255" : 10, "98173219" : 19},
+    # "Kallang Wave Mall" : {"96493305" : 40, "87083988" : 10},
+    # "Millenia Walk" : {"92245161" : 31},
+    # "Orchard Central" : {"96625428" : 6, "91164088" : 3},
+    # "Orchard Gateway" : {"99995219" : 31, "99002814" : 5},
+    # "People's Park Centre" : {"95066161" : 49},
+    # "People's Park Complex" : {"95066161" : 31},
+    # "Mustafa Shopping Centre" : {"95066161" : 3, "93861925" : 3, "95939567" : 3, "82204942" : 3},
+    # "Nex" : {"91654703" : 2},
+    # "Ngee Ann City" : {"99053469": 5, "99002814" : 5},
+    # "Northpoint" : {"99053469" : 1, "98741636" : 1, "86148198" : 1, "98439574" : 8, "98439575" : 1},
+    # "OUE Downtown" : {"94185021" : 31},
+    # "Paragon" : {"83858992" : 7, "91164088" : 7},
+    # "Paya Lebar Quarter" : {"89083988" : 4, "99124255" : 5},
+    # "Pioneer Mall" : {"92245161" : 1, "93993463" : 1},
+    # "Plaza Singapura" : {"92294434" : 1, "91806792" : 2},
+    # "PoMo" : {"90515712" : 7, "89083988" : 27, "87083988" : 6, "83858992" : 10},
+    # "Serangoon Plaza" : {"99124255" : 2},
+    # "Shaw House and Centre" : {"93861925" : 1},
+    # "Sim Lim Square" : {"96760458" : 11, "90515712" : 9},
+    # "Sim Lim Tower" : {"96760458" : 8, "90515712" : 6},
+    # "SIT@DOVER" : {"93993463" : 1},
+    # "SIT@NP" : {"96008055" : 10, "91164088" : 10},
+    # "SIT@NYP" : {"91806792" : 26, "93120759" : 1, "93690508" : 1},
+    # "SIT@RP" : {"93993463" : 3},
+    # "SIT@SP" : {"96008055" : 6, "91164088" : 6},
+    # "SIT@TP" : {"96008055" : 5, "91164088" : 5},
+    # "Sunshine Plaza" : {"82204942" : 1},
+    # "Suntec City" : {"83858992" : 40, "96625428" : 31, "86806044" : 11},
+    # "Square 2" : {"95939567" : 1},
+    # "Tampines 1" : {"96493305" : 31},
+    # "Tanglin Mall" : {"96625428" : 10, "88539489" : 8},
+    # "The Shoppes at Marina Bay Sands" : {"87686851" : 40},
+    # "The Star Vista" : {"85571447" : 7, "83638020" : 1},
+    # "Velocity@Novena Square" : {"88539489" : 31},
+    # "Vivo City" : {"82863770" : 27, "94459175" : 5},
+    # "Westgate Mall" : {"97936255" : 3,"96493305" : 3, "88539489" : 26, "96760458": 8},
+    # "Westmall" : {"82863770" : 1, "87309912" : 1, "97601182" : 4, "89652292" : 40, "91806790" : 4},
+    # "Wheelock Place" : {"87686851" : 3, "95066161" : 7, "94185021" : 49},
+    # "Zhongshan Mall" : {"91700369": 5, "82863770" : 5, "92294434" : 40, "97601182" : 4, "92245160" : 27, "83638020" : 27},
+
+
+# def dijikstra(graph,start,end,visited=[], distance = {}, predecessors = {}):
+#     if start not in graph:
+#         raise TypeError ('The root of the shortest path tree cannot be found')
+#     if end not in graph:
+#         raise TypeError ('The target of the shortest path tree cannot be found')
+#     if start == end:
+#         network = []
+#         pred = end
+#         while pred != None:
+#             network.append(pred)
+#             pred = predecessors.get(pred,None)
+        
+#         temp = network[0]
+#         for i in range(1,len(network)):
+#             temp = network[i] + '--->' + temp
+#         print('Shortest social network (in arr): ' + str(network))
+#         print('Network: ' + temp + ",    cost=" + str(distance[end]))
+#     else:
+#         if not visited:
+#             distance[start] = 0
+        
+#         for next in graph[start]:
+#             if next not in visited:
+#                 newDistance < distance.get(next,float('inf'))
+#                 distance[next] = newDistance
+#                 predecessors[next] = start 
+#         visited.append(start)
+
+#         unvisited = {}
+#         for k in graph:
+#             if k not in visited:
+#                 unvisited[k] = distance.get(k,float('inf'))
+#         x = min(unvisited, key = unvisited.get)
+#         dijikstra(graph,x,start,visited, distance,predecessors)
+
+
+# dijikstra(graph,'s', 't')
